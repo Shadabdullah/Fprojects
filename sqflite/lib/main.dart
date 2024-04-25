28a8ebb77db0b1c2d87e458f0c2da05a137b41c5
Shadabdullah <shadabdullah101@gmail.com>
2024-04-25 12:08:19 +0530

    Database setup

---
+++ sqflite/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screens/homepage.dart';
+
+void main() {
+  runApp(const Todo());
+}
+
+class Todo extends StatelessWidget {
+  const Todo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
