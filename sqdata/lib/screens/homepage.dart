a6822fe91e3db9085026c39cbc84088f399d6ef2
Shadabdullah <shadabdullah101@gmail.com>
2024-04-25 20:21:38 +0530

    Sqlite crud

---
+++ sqdata/lib/screens/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[200],
+        centerTitle: true,
+        title: Text(
+          'SQFLITE',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
