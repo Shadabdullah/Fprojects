a96c600110d815ba57346b191193279ca8929102
Shadabdullah <shadabdullah101@gmail.com>
2024-04-26 17:15:40 +0530

    ui challenge dir

--- todo/lib/screens/homepage.dart
+++ todo/lib/screens/homepage.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import '../utils/todo_db.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future _testData() async {
+    TodoDb db = TodoDb();
+    await db.database;
+    List<Todo> todos = await db.getTodos();
+    await db.deleteAll();
+    todos = await db.getTodos();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Todo",
+          "TODO BLOC",
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
---
+++ ui-challenge/flutterui/lib/consertApp/homepage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConcertHomePage extends StatelessWidget {
+  const ConcertHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
