96cb0b6e679b04d500df0f12a717f5697dd2ce64
Shadabdullah <shadabdullah101@gmail.com>
2024-04-27 06:58:17 +0530

    removed Files

--- todo/lib/screens/homepage.dart
+++ todo/lib/screens/homepage.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../utils/todo_db.dart';
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,20 +8,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future _testData() async {
-    TodoDb db = TodoDb();
-    await db.database;
-    List<Todo> todos = await db.getTodos();
-    await db.deleteAll();
-    todos = await db.getTodos();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "TODO BLOC",
+        title: const Text(
+          "TODO",
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
