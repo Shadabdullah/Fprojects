b4bcbbcb104b622cefe834edcc89a556a9a966db
Shadabdullah <shadabdullah101@gmail.com>
2024-04-27 16:16:48 +0530

    Fixed Overflow

--- todo/lib/screens/add_item.dart
+++ todo/lib/screens/add_item.dart
@@ -8,6 +8,7 @@ class AddItems extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: const Color(0xFFF1F5F9),
       appBar: AppBar(
         leading: Padding(
@@ -31,162 +32,166 @@ class AddItems extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.w800, fontSize: 16),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            const Text(
-              'Task Title',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w800,
-                  fontSize: 16),
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder()),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Row(children: [
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
               const Text(
-                'Category',
+                'Task Title',
                 style: TextStyle(
                     color: Colors.black,
                     fontWeight: FontWeight.w800,
                     fontSize: 16),
               ),
-              const SizedBox(
-                width: 20,
+              const TextField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder()),
               ),
-              Flexible(
-                flex: 1,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              const SizedBox(
+                height: 40,
+              ),
+              Row(children: [
+                const Text(
+                  'Category',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w800,
+                      fontSize: 16),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Flexible(
+                  flex: 1,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CategoryIcon(
+                            icon: Icons.dining_outlined,
+                            color: Colors.yellow[200]!),
+                        CategoryIcon(
+                            icon: Icons.cast_for_education_outlined,
+                            color: Colors.green[200]!),
+                        CategoryIcon(
+                            icon: Icons.travel_explore_outlined,
+                            color: Colors.orange[200]!),
+                        CategoryIcon(
+                            icon: Icons.local_grocery_store_outlined,
+                            color: Colors.purple[200]!),
+                        CategoryIcon(
+                            icon: Icons.health_and_safety_outlined,
+                            color: Colors.pink[200]!),
+                      ],
+                    ),
+                  ),
+                )
+              ]),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'When',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w800,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 10), // Add a SizedBox for spacing
+                  const Row(
                     children: [
-                      CategoryIcon(
-                          icon: Icons.dining_outlined,
-                          color: Colors.yellow[200]!),
-                      CategoryIcon(
-                          icon: Icons.cast_for_education_outlined,
-                          color: Colors.green[200]!),
-                      CategoryIcon(
-                          icon: Icons.travel_explore_outlined,
-                          color: Colors.orange[200]!),
-                      CategoryIcon(
-                          icon: Icons.local_grocery_store_outlined,
-                          color: Colors.purple[200]!),
-                      CategoryIcon(
-                          icon: Icons.health_and_safety_outlined,
-                          color: Colors.pink[200]!),
+                      Expanded(
+                        child: TextField(
+                          style: TextStyle(color: Colors.black87),
+                          cursorColor: Colors.black87,
+                          decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.black87)),
+                              suffixIcon: Icon(
+                                size: 35,
+                                Icons.calendar_today,
+                                color: Colors.black87,
+                              ),
+                              border: OutlineInputBorder()),
+                        ),
+                      ),
+                      SizedBox(
+                          width: 10), // Add a SizedBox for horizontal spacing
+                      Expanded(
+                        child: TextField(
+                          style: TextStyle(color: Colors.black87),
+                          cursorColor: Colors.black87,
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.black87)),
+                              suffixIcon: Icon(
+                                size: 35,
+                                Icons.schedule_outlined,
+                                color: Colors.black87,
+                              ),
+                              border: OutlineInputBorder()),
+                        ),
+                      ),
                     ],
                   ),
-                ),
-              )
-            ]),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'When',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w800,
-                    fontSize: 16,
+                  SizedBox(
+                    height: 40,
                   ),
-                ),
-                const SizedBox(height: 10), // Add a SizedBox for spacing
-                const Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        style: TextStyle(color: Colors.black87),
-                        cursorColor: Colors.black87,
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white,
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.black87)),
-                            suffixIcon: Icon(
-                              size: 35,
-                              Icons.calendar_today,
-                              color: Colors.black87,
-                            ),
-                            border: OutlineInputBorder()),
-                      ),
+                  const Text(
+                    'Notes',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w800,
+                      fontSize: 16,
                     ),
-                    SizedBox(
-                        width: 10), // Add a SizedBox for horizontal spacing
-                    Expanded(
-                      child: TextField(
-                        style: TextStyle(color: Colors.black87),
-                        cursorColor: Colors.black87,
-                        decoration: InputDecoration(
-                            fillColor: Colors.white,
-                            filled: true,
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.black87)),
-                            suffixIcon: Icon(
-                              size: 35,
-                              Icons.schedule_outlined,
-                              color: Colors.black87,
-                            ),
-                            border: OutlineInputBorder()),
-                      ),
+                  ),
+                  Container(
+                    height: 220,
+                    child: TextField(
+                      expands: true,
+                      maxLines: null,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder()),
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                const Text(
-                  'Notes',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w800,
-                    fontSize: 16,
                   ),
-                ),
-                Container(
-                  height: 220,
-                  child: TextField(
-                    expands: true,
-                    maxLines: null,
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder()),
+                  SizedBox(
+                    height: 50,
                   ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
 
-                SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Color(0xFF240A34))),
-                        onPressed: () {},
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: const Text(
-                            "Save",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20),
-                          ),
-                        )))
-              ],
-            ),
-          ],
+                  SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Color(0xFF240A34))),
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: const Text(
+                              "Save",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20),
+                            ),
+                          )))
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
