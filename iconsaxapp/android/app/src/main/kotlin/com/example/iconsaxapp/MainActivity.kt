b6efb08e0a201970e8a854549753e24aeab5062e
Shadabdullah <shadabdullah101@gmail.com>
2024-06-05 18:17:51 +0530

    removed

--- iconsaxapp/android/app/src/main/kotlin/com/example/iconsaxapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.iconsaxapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- iconsaxapp/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:iconsaxapp/app.dart';
-
-void main() {
-  runApp(const IconsaxApp());
-}
