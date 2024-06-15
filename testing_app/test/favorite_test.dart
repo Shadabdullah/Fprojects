65c6aac7b1796ecd8c45ff7d88041779f519eaad
Shadabdullah <shadabdullah101@gmail.com>
2024-06-15 20:52:50 +0530

    test

---
+++ testing_app/test/favorite_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+import 'package:testing_app/models/favorite.dart';
+
+void main() {
+  group('Testing App Provider', () {
+    var favorite = Favorites();
+    test('A new Item should be Added', () {});
+  });
+}
