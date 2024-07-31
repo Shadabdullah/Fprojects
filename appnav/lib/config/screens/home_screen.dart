87ce916370cd894b3d07702b63ff500c54a9fcc2
ShadAbdullah <shadabdullah101@gmail.com>
2024-07-31 15:34:38 +0530

    depen

---
+++ appnav/lib/config/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
